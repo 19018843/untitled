63f2571e42dc1e18927b04cd898b5fbda32ee82d
19018843 <80362250+19018843@users.noreply.github.com>
2021-05-27 00:31:53 +0200

    print_gegevens afgemaakt

--- src/print_factuur.java
+++ src/print_factuur.java
@@ -1,3 +1,11 @@
 public class print_factuur {
+    klant klant1;
+    public void print_gegevens(){
+        System.out.println(klant1.naam);
+        System.out.println(klant1.adres);
+        System.out.println(klant1.postcode);
+        System.out.println(klant1.telefoon);
+    }
+
 
 }
